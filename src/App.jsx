dce1b3e1e59340b1263ee4f2d361714cc31d0097
Princekumar <pk181299@gmail.com>
2024-01-13 10:36:34 +0530

    a button added

--- src/App.jsx
+++ src/App.jsx
@@ -57,9 +57,9 @@ function App() {
 
   return (
 
-    <div className='app bg-slate-900 p-8 flex flex-col justify-between'>
+    <div className='app bg-rose-600 p-8 flex flex-col justify-evenly'>
 
-      <h2 className='text-blue-600 font-bold text-2xl text-center mb-4'>Password Creator</h2>
+      <h2 className='text-white font-bold text-2xl text-center mb-4'>Password Creator</h2>
 
       <div className='my-6 flex justify-center items-center'>
         <input
@@ -76,7 +76,7 @@ function App() {
       </div>
 
 
-      <div className='w-[100%] flex gap-2 py-10 justify-evenly md:justify-between flex-col md:flex-row'>
+      <div className='w-[100%] py-10 flex gap-2  justify-evenly md:justify-center flex-col md:flex-row'>
 
         <div className='flex gap-2 text-white flex-col md:flex-row'>
           <input type="range" id="length" max={20} value={length} onChange={(e) => setLength(e.target.value)} />
@@ -94,6 +94,8 @@ function App() {
         </div>
 
       </div>
+
+      <button onClick={passwordGenerate} className='bg-white w-[150px] mx-auto px-4 py-2 text-xl text-blue-600 font-bold rounded-full my-6 border-none hover:bg-blue-900 hover:text-white transition-all duration-75'>New</button>
 
     </div>
   )
